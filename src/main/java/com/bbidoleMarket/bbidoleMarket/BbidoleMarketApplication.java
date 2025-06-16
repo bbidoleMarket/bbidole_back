7e6023f66302cca3240b33433010fcb95cae04ee
Jungsang Gi <wjdtkdrl15432@gmail.com>
2025-06-16 14:31:55 +0900

    first commit

---
+++ src/main/java/com/bbidoleMarket/bbidoleMarket/BbidoleMarketApplication.java
@@ -0,0 +1,13 @@
+package com.bbidoleMarket.bbidoleMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbidoleMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbidoleMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bbidoleMarket/bbidoleMarket/BbidoleMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbidoleMarket.bbidoleMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbidoleMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
