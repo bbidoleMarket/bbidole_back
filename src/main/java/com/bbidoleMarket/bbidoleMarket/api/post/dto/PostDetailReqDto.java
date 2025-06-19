5899c4f1c46d54023b5c75c3a6de441bcfdd8fc0
parkjiwon <ji11won@naver.com>
2025-06-19 17:55:09 +0900

    Feat: Dto 생성
    - Post 관련 : PostSimpleDto, PostDetailResDto, PostDetailReqDto
    - User 관련 : UserDetailReqDto
    - Review 관련 : ReviewDto

---
+++ src/main/java/com/bbidoleMarket/bbidoleMarket/api/post/dto/PostDetailReqDto.java
@@ -0,0 +1,19 @@
+package com.bbidoleMarket.bbidoleMarket.api.post.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class PostDetailReqDto {
+
+    private long postId;
+    private String title;
+    private String description;
+    private int price;
+    private String imageUrl;
+}
