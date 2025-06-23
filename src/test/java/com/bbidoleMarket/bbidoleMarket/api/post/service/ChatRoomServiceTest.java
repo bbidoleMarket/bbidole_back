66b8592f02d8908b5d9dc4c07f533cb39709c018
parkjiwon <ji11won@naver.com>
2025-06-24 08:59:16 +0900

    Test: ChatRoomService 테스트 생성

---
+++ src/test/java/com/bbidoleMarket/bbidoleMarket/api/post/service/ChatRoomServiceTest.java
@@ -0,0 +1,50 @@
+package com.bbidoleMarket.bbidoleMarket.api.post.service;
+
+import com.bbidoleMarket.bbidoleMarket.api.post.dto.ChatRoomReqDto;
+import com.bbidoleMarket.bbidoleMarket.api.post.dto.ChatRoomResDto;
+import com.bbidoleMarket.bbidoleMarket.api.post.dto.PostSaveReqDto;
+import com.bbidoleMarket.bbidoleMarket.api.post.dto.SigninDto;
+import com.bbidoleMarket.bbidoleMarket.common.TestUtils;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+public class ChatRoomServiceTest {
+
+    @Autowired
+    private ChatRoomService chatRoomService;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private PostService postService;
+
+    @Test
+    public void 채팅방_생성_테스트() throws Exception {
+        // given
+        List<SigninDto> users = TestUtils.signinMany(2);
+        Long sellerId = userService.signin(users.get(0));
+        Long buyerId = userService.signin(users.get(1));
+
+        PostSaveReqDto post = TestUtils.createPostOne(sellerId);
+        Long postId = postService.save(post);
+
+        ChatRoomReqDto dto = new ChatRoomReqDto();
+        dto.setProductId(postId);
+        dto.setBuyerId(buyerId);
+        dto.setSellerId(sellerId);
+
+        // when
+        Long chatRoomId = chatRoomService.startChatRoom(dto);
+
+        // then
+        ChatRoomResDto chatRoomResDto = chatRoomService.findById(chatRoomId);
+        Assertions.assertEquals(chatRoomResDto.getProductId(), postId);
+        Assertions.assertEquals(chatRoomResDto.getBuyerId(), buyerId);
+        Assertions.assertEquals(chatRoomResDto.getSellerId(), sellerId);
+    }
+}
