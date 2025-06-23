787bdd31c25d242c846008911d1b426f4fd306e4
parkjiwon <ji11won@naver.com>
2025-06-23 11:27:50 +0900

    Feat: Swagger 설명 추가

--- src/main/java/com/bbidoleMarket/bbidoleMarket/api/health/controller/HealthController.java
+++ src/main/java/com/bbidoleMarket/bbidoleMarket/api/health/controller/HealthController.java
@@ -3,19 +3,18 @@ package com.bbidoleMarket.bbidoleMarket.api.health.controller;
 import com.bbidoleMarket.bbidoleMarket.common.reponse.ApiResponse;
 import com.bbidoleMarket.bbidoleMarket.common.reponse.SuccessStatus;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "HealthCheck", description = "HealthCheck 관련 API 입니다.")
+@Tag(name = "HealthCheck", description = "HealthCheck 관련 API 입니다.") // Swagger
 @RestController
 public class HealthController {
 
     @GetMapping("/health")
-    @Operation(summary = "서버 상태 체크 API입니다.")
-    public ResponseEntity<ApiResponse<Void>> healthCheck(){
+    @Operation(summary = "서버 상태 체크 API입니다.") // Swagger
+    public ResponseEntity<ApiResponse<Void>> healthCheck() {
         return ApiResponse.success_only(SuccessStatus.SEND_HEALTH_SUCCESS);
     }
 }
--- src/main/java/com/bbidoleMarket/bbidoleMarket/api/post/controller/ReviewController.java
+++ src/main/java/com/bbidoleMarket/bbidoleMarket/api/post/controller/ReviewController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "", description = "")
 @RestController
 @RequestMapping("/api/review")
 @RequiredArgsConstructor
@@ -21,7 +22,8 @@ public class ReviewController {
     private final ReviewService reviewService;
 
     @GetMapping("/{userId}")
-    public ResponseEntity<ApiResponse<PageResDto<ReviewDto>>> getReviewWithSeller(
+    @Operation(summary = "특정 사용자의 리뷰를 조회")
+    public ResponseEntity<ApiResponse<PageResDto<ReviewResDto>>> getReviewWithSeller(
         @PathVariable Long userId,
         @RequestParam int page,
         @RequestParam int size
