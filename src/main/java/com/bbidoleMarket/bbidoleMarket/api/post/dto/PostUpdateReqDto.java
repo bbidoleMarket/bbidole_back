98ca46eb8b3e3e65ba66c7565100669f2bb03f23
parkjiwon <ji11won@naver.com>
2025-06-23 11:29:50 +0900

    Feat:  PostSaveReq Validation 코드 추가

--- src/main/java/com/bbidoleMarket/bbidoleMarket/api/post/dto/PostUpdateReqDto.java
+++ src/main/java/com/bbidoleMarket/bbidoleMarket/api/post/dto/PostUpdateReqDto.java
@@ -1,5 +1,7 @@
 package com.bbidoleMarket.bbidoleMarket.api.post.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -11,10 +13,22 @@ import lombok.ToString;
 @NoArgsConstructor
 public class PostUpdateReqDto {
 
+    @NotNull
+    @Positive
     private long postId;
+
+    @NotNull
     private String title;
+
     private String description;
+
+    @Positive
+    @NotNull
     private int price;
-    private String imageUrl;
+
+    private String imageUrl; // TODO MultiPart로 변경
+
+    @Positive
+    @NotNull
     private long userId;
 }
