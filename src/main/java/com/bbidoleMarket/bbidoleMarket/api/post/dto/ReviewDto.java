3a1aec60d0f29e194af35ad33a2f1d63c275125b
parkjiwon <ji11won@naver.com>
2025-06-23 10:13:48 +0900

    Feat : dto 수정
    - ReviewDto : Review -> ReviewDto 변경 함수
    - SellerDetailResDto: UserDetailReqDto -> SellerDetailResDto 이름 변경, field 삭제

--- src/main/java/com/bbidoleMarket/bbidoleMarket/api/post/dto/ReviewDto.java
+++ src/main/java/com/bbidoleMarket/bbidoleMarket/api/post/dto/ReviewDto.java
@@ -1,5 +1,6 @@
 package com.bbidoleMarket.bbidoleMarket.api.post.dto;
 
+import com.bbidoleMarket.bbidoleMarket.api.entity.Review;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,5 +16,14 @@ public class ReviewDto {
     private String writerNickname;
     private String content;
     private int rating;
+
+    public static ReviewDto fromReview(Review review) {
+        ReviewDto dto = new ReviewDto();
+        dto.setId(review.getId());
+        dto.setWriterNickname(review.getReviewer().getNickname());
+        dto.setContent(review.getContent());
+        dto.setRating(review.getRating());
+        return dto;
+    }
 
 }
