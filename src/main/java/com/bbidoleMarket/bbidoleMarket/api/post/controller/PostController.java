c3bc00a4a88bdd3a68294128303cfc56291acba4
parkjiwon <ji11won@naver.com>
2025-06-25 15:33:56 +0900

    Revert "Revert 'Merge branch 'feat/post' into dev'"

---
+++ src/main/java/com/bbidoleMarket/bbidoleMarket/api/post/controller/PostController.java
@@ -0,0 +1,71 @@
+package com.bbidoleMarket.bbidoleMarket.api.post.controller;
+
+import com.bbidoleMarket.bbidoleMarket.api.post.dto.PageResDto;
+import com.bbidoleMarket.bbidoleMarket.api.post.dto.PostDetailResDto;
+import com.bbidoleMarket.bbidoleMarket.api.post.dto.PostSimpleDto;
+import com.bbidoleMarket.bbidoleMarket.api.post.dto.PostUpdateReqDto;
+import com.bbidoleMarket.bbidoleMarket.api.post.service.PostService;
+import com.bbidoleMarket.bbidoleMarket.common.reponse.ApiResponse;
+import com.bbidoleMarket.bbidoleMarket.common.reponse.SuccessStatus;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@Tag(name = "", description = "")
+@Slf4j
+@RequestMapping("/api/post")
+@RequiredArgsConstructor
+@RestController
+public class PostController {
+
+    private final PostService postService;
+
+    @GetMapping("/{id}")
+    @Operation(summary = "게시물 상세 내용 조회입니다.")
+    public ResponseEntity<ApiResponse<PostDetailResDto>> findById(@PathVariable Long id) {
+        return ApiResponse.success(SuccessStatus.SEARCH_POST_SUCCESS, postService.findById(id));
+    }
+
+    /*// TODO 삭제
+    @PostMapping("/")
+    public ResponseEntity<ApiResponse<String>> save(@RequestBody PostSaveReqDto dto) {
+        postService.save(dto);
+        return ApiResponse.success(SuccessStatus.SEARCH_POST_SUCCESS, "게시물 저장에 성공했습니다.");
+    }*/
+
+    // HTTP(Header:Content-Type) 설정 : consumes, produces
+    @PutMapping(value = "/", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(summary = "게시물 수정 입니다.")
+    public ResponseEntity<ApiResponse<Void>> update(
+        @RequestPart(name = "image", required = false) MultipartFile image,
+        @RequestPart(name = "dto") PostUpdateReqDto dto
+    ) {
+        postService.update(dto, image);
+        return ApiResponse.success_only(SuccessStatus.UPDATE_POST_SUCCESS);
+    }
+
+    @GetMapping("/seller/{id}")
+    @Operation(summary = "판매자의 판매중인 게시물의 조회입니다.")
+    public ResponseEntity<ApiResponse<PageResDto<PostSimpleDto>>> findAllBySellerId(
+        @PathVariable Long id,
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "20") int size
+    ) {
+        return ApiResponse
+            .success(SuccessStatus.SEARCH_POST_SUCCESS, postService.findByUserId(id, page, size));
+
+    }
+
+
+}
---
+++ src/main/java/com/bbidoleMarket/bbidoleMarket/api/post/dto/UserDetailResDto.java
@@ -0,0 +1,19 @@
+package com.bbidoleMarket.bbidoleMarket.api.post.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class UserDetailResDto {
+
+    private long userId;
+    private String nickname;
+    private String imageUrl;
+    private Double totalRating;
+
+}
