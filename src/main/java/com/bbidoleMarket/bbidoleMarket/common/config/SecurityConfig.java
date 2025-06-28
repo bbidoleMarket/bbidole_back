d337380908628e57277e048294882fe204f0e358
parkjiwon <ji11won@naver.com>
2025-06-28 23:02:03 +0900

    Fix: /없어서 생기는 warn 해결

--- src/main/java/com/bbidoleMarket/bbidoleMarket/common/config/SecurityConfig.java
+++ src/main/java/com/bbidoleMarket/bbidoleMarket/common/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.bbidoleMarket.bbidoleMarket.common.config;
 
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,12 +14,12 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
     private final JwtAuthFilter jwtAuthFilter;
+
     public SecurityConfig(JwtAuthFilter jwtAuthFilter) {
         this.jwtAuthFilter = jwtAuthFilter;
     }
@@ -25,31 +27,35 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/auth/login", "api/posts","api/posts/search","/api/auth/signup", "/api/auth/check-email", "/api/auth/check-nickname", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-config", "/api-doc", "/ws/**", "/api/post/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .formLogin(login -> login.disable())
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))  // CORS 설정 적용
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers("/api/auth/login", "/api/posts", "/api/posts/search",
+                    "/api/auth/signup", "/api/auth/check-email", "/api/auth/check-nickname",
+                    "/v3/api-docs/**", "/swagger-ui/**", "/swagger-config", "/api-doc", "/ws/**",
+                    "/api/post/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .formLogin(login -> login.disable())
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))  // CORS 설정 적용
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
     /**
-     * CORS 설정을 위한 Bean
-     * 다른 출처(Origin)에서의 API 요청을 허용하기 위한 설정입니다.
-     * 
+     * CORS 설정을 위한 Bean 다른 출처(Origin)에서의 API 요청을 허용하기 위한 설정입니다.
+     *
      * @return CORS 설정 소스
      */
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList(
+        configuration.setAllowedOrigins(List.of(
             "http://localhost:5173"
         ));  // 프론트엔드 서버 Origin 명시
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));  // 허용할 HTTP 메서드
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Refresh-Token"));  // 허용할 헤더
+        configuration.setAllowedMethods(
+            Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));  // 허용할 HTTP 메서드
+        configuration.setAllowedHeaders(
+            Arrays.asList("Authorization", "Content-Type", "Refresh-Token"));  // 허용할 헤더
         configuration.setAllowCredentials(true);  // 쿠키 및 인증 정보 허용
         configuration.setMaxAge(3600L);  // 1시간 동안 preflight 요청 캐싱
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
